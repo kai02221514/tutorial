7d77e1a44a217b4f1c5fe3a472e8956c6d56149e
北代櫂 <0jr0572.7264b3x@ezweb.ne.jp>
2021-01-14 17:14:10 +0900

    条件分岐 (if) と繰り返し (loop)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useState } from "react";
 import { List } from "./Components/List"
+import { Form } from "./Components/Form"
 
 function App() {
-  const [description,setDescription] = useState('クリック前');
+  const [tab,setTab] = useState('list');
 
-  const changeDeskription = () => {
-    setDescription('クリック後');
-  }
+
   return (
 <div>
-  { description }
-  <List title="取扱言語一覧" />
-  <button onClick={changeDeskription}>ボタン</button>
+  <header>
+    <ul>
+      <li onClick={() => setTab('list')}>list</li>
+      <li onClick={() => setTab('form')}>form</li>
+    </ul>
+  </header>
+  <hr/>
+  {
+    tab === 'list' ? <List /> : <Form />
+  }
 </div>
   );
 }
+
 
 export default App;
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,8 +1,19 @@
-export const List = ({ title }) => {
+const Language = [
+    'Java',
+    'C++',
+    'PHP'
+];
+
+export const List = () => {
     return(
         <div>
-        <h4>{ title }</h4>
-        <div>リストです。</div>
+        
+        
+        {
+            Language.map((lang,index) => {
+                return <div key={index}>{ lang }</div>
+            })
+        }
         </div>
     )
 }
